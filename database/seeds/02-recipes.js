20aa4f2c7c3f6e2b07f53415594659ddd608048f
Leelsmuth <femianjorin@rocketmail.com>
2019-11-18 16:15:35 +0100

    refactored code in recipe-seed

--- database/seeds/02-recipes.js
+++ database/seeds/02-recipes.js
@@ -1,7 +1,7 @@
 exports.seed = function(knex) {
   // Deletes ALL existing entries
   return knex("recipes")
-    .delete()
+    .del()
     .then(function() {
       // Inserts seed entries
       return knex("recipes").insert([
