e1298227cfe177d378a3a031c857bd8cf11886ba
Leelsmuth <femianjorin@rocketmail.com>
2019-11-18 18:14:59 +0100

    installed knex-cleaner,update seed files

--- database/seeds/01-users.js
+++ database/seeds/01-users.js
@@ -1,13 +1,7 @@
 exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  return knex("users")
-    .del()
-    .then(function() {
-      // Inserts seed entries
-      return knex("users").insert([
-        { username: "user1", email: "user1@yahoo.com", password: "1234" },
-        { username: "user2", email: "user2@yahoo.com", password: "1234" },
-        { username: "user3", email: "user3@yahoo.com", password: "1234" }
-      ]);
-    });
+  return knex("users").insert([
+    { username: "user1", email: "user1@yahoo.com", password: "1234" },
+    { username: "user2", email: "user2@yahoo.com", password: "1234" },
+    { username: "user3", email: "user3@yahoo.com", password: "1234" }
+  ]);
 };
--- database/seeds/02-recipes.js
+++ database/seeds/02-recipes.js
@@ -1,33 +1,27 @@
 exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  return knex("recipes")
-    .del()
-    .then(function() {
-      // Inserts seed entries
-      return knex("recipes").insert([
-        {
-          recipe_name: "Pepperoni Pizza",
-          recipe_source: "Evil Grandma",
-          recipe_ingredients:
-            "tomatoes, salt, cheese, flour, black pepper, sugar, garlic powder, onion powder, magic wand, garlic potion ",
-          recipe_category: "dinner",
-          user_id: 1
-        }, // id 1 will be generated
-        {
-          recipe_name: "Pork Tacos",
-          recipe_source: "Annoying Aunty",
-          recipe_ingredients:
-            "shrimp, salt, beef, roasted veggies, shredded chicken, some bits of annoyance ",
-          recipe_category: "dessert",
-          user_id: 2
-        }, //id  2
-        {
-          recipe_name: "Hot Dog",
-          recipe_source: "Grinchy Cousin",
-          recipe_ingredients: "sausage, bread, mustard, salt, Grinchy ketchup",
-          recipe_category: "snack",
-          user_id: 3
-        } //id  3
-      ]);
-    });
+  return knex("recipes").insert([
+    {
+      recipe_name: "Pepperoni Pizza",
+      recipe_source: "Evil Grandma",
+      recipe_ingredients:
+        "tomatoes, salt, cheese, flour, black pepper, sugar, garlic powder, onion powder, magic wand, garlic potion ",
+      recipe_category: "dinner",
+      user_id: 1
+    }, // id 1 will be generated
+    {
+      recipe_name: "Pork Tacos",
+      recipe_source: "Annoying Aunty",
+      recipe_ingredients:
+        "shrimp, salt, beef, roasted veggies, shredded chicken, some bits of annoyance ",
+      recipe_category: "dessert",
+      user_id: 2
+    }, //id  2
+    {
+      recipe_name: "Hot Dog",
+      recipe_source: "Grinchy Cousin",
+      recipe_ingredients: "sausage, bread, mustard, salt, Grinchy ketchup",
+      recipe_category: "snack",
+      user_id: 3
+    } //id  3
+  ]);
 };
